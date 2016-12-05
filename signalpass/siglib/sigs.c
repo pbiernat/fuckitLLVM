374882024a375bc647354e4bc4a9e92db5e657ce
pbiernat <biernp@rpi.edu>
2016-12-05 04:17:54 -0500

    Add signal stuff

---
+++ signalpass/siglib/sigs.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#define __USE_GNU
+#include <signal.h>
+
+#include <string.h>
+
+#include <unistd.h>
+#include <ucontext.h>
+#include <sys/types.h>
+
+
+long long * find_reasonable_return_address(long long * stack);
+
+static void sighandle(int sig, siginfo_t *siginfo, void *context) {
+    pid_t my_pid;  
+    ucontext_t *uc = (ucontext_t *)context;
+   
+    printf("Sending PID: %ld, UID: %ld, Signal: %d\n",
+            (long)siginfo->si_pid, (long)siginfo->si_uid, sig);
+    
+    my_pid = getpid();
+
+
+    if ( (siginfo->si_signo == SIGTERM) ) {
+        printf("Got Sigterm\n");
+        if ( (my_pid != siginfo->si_pid) && (siginfo->si_pid != -7)) {
+            // wanna fite m8?    
+            kill(siginfo->si_pid, SIGKILL);
+            return;
+        }
+    }
+    
+    if ( (siginfo->si_signo == SIGILL) ) {
+        printf("Got Sigill\n");
+        uc->uc_mcontext.gregs[REG_RIP] += 1;
+    }
+    
+    if ( (siginfo->si_signo == SIGABRT) ) {
+        long long * stack;
+        /*
+            Lots of libc stuff ends up in SIGABRT if you force things forward enough
+            e.g free. Quitting is for losers so we're gonna find a return address and just
+            go with it.
+        */
+       
+        // Most of the time returning 0 means stuff is ok.
+        uc->uc_mcontext.gregs[REG_RAX] = 0;
+
+        printf("Got SigAbort\n");
+        printf("RIP at %p\n", uc->uc_mcontext.gregs[REG_RIP]);
+
+        // Swap out rip
+        stack = find_reasonable_return_address(uc->uc_mcontext.gregs[REG_RSP]);
+        uc->uc_mcontext.gregs[REG_RIP] = *stack;
+        
+
+        // Hope rbp is there
+        stack--;
+        uc->uc_mcontext.gregs[REG_RSP] = *stack;
+
+        printf("Set Stack to %p\n", *stack);
+
+        setcontext(uc);
+        return;
+    }
+
+    if (siginfo->si_signo == SIGSEGV) {
+        // its fine we'll just skip that one.
+        long long newRIP;
+        printf("Segfault at address: %p\n", siginfo->si_addr);
+        printf("RIP at %p\n", uc->uc_mcontext.gregs[REG_RIP]);
+        // YOLO
+        uc->uc_mcontext.gregs[REG_RIP] += 1;
+
+    }
+
+}
+
+long long * find_reasonable_return_address(long long * stack) {
+    long long myaddr;
+    int j;
+    myaddr = (long long)&sighandle & 0xffffffffffff0000;
+    printf("Stack is @: %p\n", stack);
+
+    for (stack;;stack++) {
+        printf("Stack: %p\n\tValue: %p\n", stack, *stack);
+        if ( (*stack & 0xffffffffffff0000) == myaddr ) {
+            for(j = 0; j < 0x10; j++) {
+                printf("Found %p\n", *stack);
+                if (memcmp( (void *)(*stack) + j, "\x41\x41\x41\x41", 4) == 0) {
+                    // Probably the right one
+                    printf("Returning to: %p\n",  *stack);
+                    return (long long)stack;
+                }
+            }
+        }
+    }
+}
+
+
+
+void register_sighandlers() {
+    struct sigaction act;
+
+	memset (&act, '\0', sizeof(act));
+ 
+	/* Use the sa_sigaction field because the handles has two additional parameters */
+	act.sa_sigaction = &sighandle;
+ 
+	/* The SA_SIGINFO flag tells sigaction() to use the sa_sigaction field, not sa_handler. */
+	act.sa_flags = SA_SIGINFO;
+ 
+	if (sigaction(SIGTERM, &act, NULL) < 0) {
+		perror ("sigaction");
+		return;
+	}
+	
+	if (sigaction(SIGSEGV, &act, NULL) < 0) {
+		perror ("sigaction");
+		return;
+	}
+	
+    if (sigaction(SIGILL, &act, NULL) < 0) {
+		perror ("sigaction");
+		return;
+	}
+    
+    if (sigaction(SIGABRT, &act, NULL) < 0) {
+		perror ("sigaction");
+		return;
+	}
+
+    printf("Fuck it, loaded handlers\n");
+
+}
---
+++ signalpass/sigpass.cpp
@@ -0,0 +1,69 @@
+#include <llvm/IR/InstVisitor.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/IRBuilder.h>
+
+using namespace llvm;
+
+struct sigpass : public FunctionPass
+{
+	static char ID;
+	sigpass() : FunctionPass(ID) {}
+
+    int insert_sighandlers(Function * F) {
+        // Get the Context
+        LLVMContext& ctx = F->getContext();
+        Constant * sigFunc = F->getParent()->getOrInsertFunction(
+                                        "register_sighandlers", Type::getVoidTy(ctx),
+                                        NULL);
+       
+        IRBuilder<> builder(F->getEntryBlock().getFirstNonPHI());
+        //builder.SetInsertPoint(F->getEntryBlock(), ++builder.GetInsertPoint());
+        Value * args[] = {0};
+        builder.CreateCall(sigFunc);
+
+        errs() << "Created Builder!\n";
+        return 0;
+    }
+
+    int insert_callmarkers(Function * F) {
+        Value * junk1 = llvm::ConstantInt::get(F->getContext(), llvm::APInt(64, 0x31337, false));
+        Value * junk2 = llvm::ConstantInt::get(F->getContext(), llvm::APInt(64, 0, false));
+
+        for (Function::iterator bb=F->begin(), bb_end=F->end(); bb != bb_end; ++bb) {
+            for (BasicBlock::iterator inst=bb->begin(), end=bb->end(); inst != end; ++inst) {
+                if (isa<CallInst>(*inst)) {
+                    errs() << "Found Call Instruction: Placing marker...\n";
+                    BasicBlock::iterator itt(*inst);
+                    itt++;
+                    IRBuilder<> builder(&(*itt));
+
+                    Constant* beginConstAddress = ConstantInt::get(Type::getInt64Ty(F->getContext()), (int64_t)0x41414141);
+                    Value* ConstPtr = ConstantExpr::getIntToPtr(
+                    beginConstAddress , PointerType::getUnqual(Type::getInt64Ty(F->getContext()))); 
+
+
+
+                    Value * lol = builder.CreateLoad(ConstPtr);
+                    builder.CreateStore(lol, ConstPtr);
+                }
+            }
+        }
+        return 0;
+    }
+
+	virtual bool
+	runOnFunction(Function &F)
+	{
+
+        insert_callmarkers(&F);
+        
+        if (F.getName() == "main") {
+            errs() << "Identified Main\n";
+            insert_sighandlers(&F);
+        }
+		return true;
+	}
+};
+
+char sigpass::ID = 0;
+static RegisterPass<sigpass> cats("sigpass", "Improve reliability", false, false);
